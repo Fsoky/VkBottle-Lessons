1d47271d5b155fcf4653e0346aaf0fea9146c685
Fsoky Community <61309653+Fsoky@users.noreply.github.com>
2022-01-12 03:41:33 +0600

    Update commands_for_chat.py

--- source code/vkbottle_bot_5/blueprints/commands_for_chat.py
+++ source code/vkbottle_bot_5/blueprints/commands_for_chat.py
@@ -14,5 +14,5 @@ async def kick_handler(message: Message, member=None):
 		try:
 			member = re.findall(r"[0-9]+", member)[0]
 			await bp.api.messages.remove_chat_user(message.chat_id, int(member))
-		except VKAPIError(15):
+		except VKAPIError[15]:
 			await message.answer("Недостаточно прав...")
